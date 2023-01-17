8f2db31c5e644181ac2997d3b2ecdd9cdaa369cd
Olivia Klepacka <olivia.klepacka@gmail.com>
2023-01-17 21:17:30 +0100

    before sass

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const dots = Array.from(dotsNav.children);
 const slideSize = slides[0].getBoundingClientRect();
 const slideWidth = slideSize.width;
 
-console.log(slides);
-
 ///arange the slide 
+
+
 
 ///slides.forEach((slide,index) => {
 //  slide.style.left = slideSize * index + 'px';
@@ -27,19 +27,75 @@ slides.forEach((slide,index) => {
   slide.style.left = slideWidth * index + 'px';
 })
 
-nextButton.addEventListener('click', e => {
-	const currentSlide = track.querySelector('.current-slide');
-	const nextSlide = currentSlide.nextElementSibling;
-  const amountToMove = nextSlide.style.left;
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');  
+}
 
-  track.style.transform = 'translateX(-' + amountToMove + ')';
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else if (targetIndex === slides.length -1) {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+  } else {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  }
+}
+
+
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+	const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowArrows(slides, prevButton, nextButton, prevIndex);
 
 })
 
+nextButton.addEventListener('click', e => {
+	const currentSlide = track.querySelector('.current-slide');
+	const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide);
 
-//when I click left, move slides to the left
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, prevButton, nextButton, nextIndex);
 
-//when I click right..
+})
+
+//indicators
+
+dotsNav.addEventListener('click', e => {
+  //what indicator was clicked on?
+  const targetDot = e.target.closest('button');
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot   = dotsNav.querySelector('.current-slide');
+  const targetIndex  = dots.findIndex(dot => dot ===targetDot);
+  const targetSlide  = slides[targetIndex];
+
+  moveToSlide(track,currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  hideShowArrows(slides, prevButton, nextButton, targetIndex);
+  
+})
 
 
 
@@ -70,4 +126,4 @@ const hideMobileMenu = () => {
 };
 
 menuLinks.addEventListener('click', hideMobileMenu);
-//navLogo.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
